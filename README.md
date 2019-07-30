d76cf735976f24b66afb8715dd30a556fb5a67be
matt6frey <matt6frey@gmail.com>
2019-07-29 21:04:34 -0600

    Refactored some of the logic in getData, added more information to Readme, and enabled tags (filename, dirm, ext) to be callable from command line.

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Python Stock Screener
 
-This is a stock screener that harnesses the Finviz Repository (), it then uses Beautiful Soup collect data and export it into a CSV file.
-The screener settings can be changed and by using the options provided below:
+This is a stock screener that harnesses the Finviz Module ([FinViz - Repo][https://github.com/mariostoev/finviz], [Finviz - PiPy][https://pypi.org/project/finviz/]), it then uses Beautiful Soup collect data and export it into a CSV file.
+
+## Installation & Usage
+
+**Install**
+
+`pip install -r requirements.txt`
 
 
+**Usage**
 
+`python yahooScreener.py` in your terminal. The script will save the results within the same directory, inside a CSV called 'gappers.csv'. 
+
+Alternatively, you can use the `--filename=<filename>` to change the filename, `--dir=<dir>` to change the directory path and `--ext=<ext>` to change the extension of the file.
+
+i.e. `python yahooScreener.py --filename=stocks --dir=~/Desktop/Stocks/ --ext=txt`
+
+## Screener Options
+
+*Coming Soon*
+
+## Modules
+
+- FinViz
+- BeautifulSoup
+- URLLIB
--- yahooScreener.py
+++ yahooScreener.py
@@ -1,8 +1,33 @@
-import os, datetime, re
+import os, datetime, re, sys
 from time import sleep
 from finviz.screener import Screener
 from urllib import request as r
 from bs4 import BeautifulSoup
+
+def check_args(args, flag):
+    '''Cycles through sys args and returns None or value of Arg'''
+    value = None
+    if len(args):
+        for arg in args:
+            if flag in arg:
+                value = get_arg(arg)
+    return value
+
+def get_arg(arg):
+    return arg.split('=')[1] if arg else ''
+
+filename = check_defaults(check_args(sys.argv, 'filename'),'filename')
+path = check_defaults(check_args(sys.argv, 'dir'),'dir')
+ext = check_defaults(check_args(sys.argv, 'ext'),'ext')
+target_path = f'{path}{filename}.{ext}'
+
+def check_defaults(value, flag):
+    defaults = {
+        'filename': 'gappers',
+        'dir': os.getcwd(),
+        'ext':'csv'
+    }
+    return value if value != None else defaults[flag]
 
 # Filter for Stocks
 filters = ['sh_price_u5','ta_gap_d5','ta_rsi_os30','ft=3']
@@ -10,10 +35,18 @@ stocks = Screener(filters=filters, order="price")
 tickers = [ticker['Ticker'] for ticker in stocks]
 
 def group(lst, n):
+  '''Creates an Array of 2 values: key & value from BS4 extracted data.'''
   for i in range(0, len(lst), n):
     val = lst[i:i+n]
     if len(val) == n:
       yield tuple(val)
+
+def scan_columns(value):
+    '''Verify Column is present from Current TD element.'''
+    columns = ['Open','Close','Volume','Avg. Volume','Bid','Ask']
+
+    for col in columns:
+        return True if col in value
 
 def getData(tickers, ctime):
     for ticker in tickers:
@@ -21,9 +54,9 @@ def getData(tickers, ctime):
         soup = BeautifulSoup(html, 'html.parser')
 
         quoteHeader = soup.findAll('div', attrs={"id":"quote-header-info"})
-        current_price = quoteHeader[0].findAll(attrs={"data-reactid":"14"})[0].get_text()
+        current_price = quoteHeader[0].findAll(attrs={"data-reactid":"14"})[0].get_text() # specific TD from Yahoo Finance.
         tds = list(group(soup.findAll(lambda tag: tag.name == 'td' and 'data-reactid' in tag.attrs), 2))
-        texts = { t[0].get_text(): t[1].get_text() for t in tds if 'Open' in t[0].get_text() or 'Volume' in t[0].get_text() or 'Avg. Volume' in t[0].get_text() or 'Bid' in t[0].get_text() or 'Ask' in t[0].get_text()}
+        texts = { t[0].get_text(): t[1].get_text() for t in tds if scan_columns(t[0].get_text()) }
 
         data = {
             "current time": ctime.strftime("%Y %m %d - %H:%M:%S"),
@@ -37,20 +70,23 @@ def getData(tickers, ctime):
             "close": current_price if ctime.hour == 17 or 19 else None
         }
 
-        gappers = open('gappers.csv', 'a+')
-        gappers_read = open('gappers.csv','r')
-        lines = len(gappers_read.readlines())
-        gappers_read.close()
-        if lines > 0:
-            gappers.write(f"{','.join(data.values())}\n")
+        if os.path.exists(path):
+            gappers = open(target_path, 'a+')
+            gappers_read = open(target_path,'r')
+            lines = len(gappers_read.readlines())
+            gappers_read.close()
+            if lines > 0:
+                gappers.write(f"{','.join(data.values())}\n")
+            else:
+                gappers.write(f"{','.join(data.keys())}\n")
+                gappers.write(f"{','.join(data.values())}\n")
+            print(f'recorded data for time { ctime }')
         else:
-            gappers.write(f"{','.join(data.keys())}\n")
-            gappers.write(f"{','.join(data.values())}\n")
-        print(f'recorded data for time { ctime }')
+            raise OSError(f"Path: {target_path} doesn't exist...")
 
 def recordData():
     check = True
-    # adjusted to MST, no daylight savings time included.
+    # adjusted from EST to MST, no daylight savings time included.
     while check:
         current_time = datetime.datetime.now()
         if current_time.hour >= 7 and current_time.hour <= 13:
